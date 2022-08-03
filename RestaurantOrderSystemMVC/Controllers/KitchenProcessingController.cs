780536e4288cab43de3bdd344caa9b000a9afe48
robertmoran0002 <59235360+robertmoran0002@users.noreply.github.com>
2022-08-03 17:58:41 -0600

    KitchenOrdering in progress

--- RestaurantOrderSystemMVC/Controllers/KitchenProcessingController.cs
+++ RestaurantOrderSystemMVC/Controllers/KitchenProcessingController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RestaurantOrderSystem.Models;
+using RestaurantOrderSystemMVC.Data;
 
 namespace RestaurantOrderSystemMVC.Controllers
 {
 	public class KitchenProcessingController : Controller
 	{
-		public IActionResult Index()
-		{
-			return View();
-		}
+        private readonly ApplicationDbContext _context;
+        public KitchenProcessingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OrderProcess()
+        {
+            ViewData["MenuData"] = _context.Menus.ToList();
+            ViewData["MenuList"] = new SelectList(_context.Menus, "ItemId", "Name");
+            ViewData["OrderData"] = _context.OrderMains.ToList();
+            ViewData["OrderList"] = new SelectList(_context.OrderMains, "ItemId", "Quantity", "DateTimePlaced", "OrderNumber");
+            ViewData["blah"] = new MultiSelectList(_context.OrderMains, "ItemId", "Quantity", "DateTimePlaced", "OrderNumber");
+            ViewData["test"] = ViewData["OrderList"];
+            //ViewData["OrderList"] = new List<OrderMain>(_context.OrderMains);
+            return View();
+        }
+  //      public IActionResult Index()
+		//{
+		//	return View();
+		//}
 	}
 }
+
+
