4af36f1e1ffcacad179408156f9afb778f62d6d5
John <john.yang559@yahoo.com>
2022-08-03 19:48:33 -0700

    Add to Order is fully functional. Also added Sweet alert functionalities.

--- RestaurantOrderSystemMVC/Controllers/AddToOrderController.cs
+++ RestaurantOrderSystemMVC/Controllers/AddToOrderController.cs
@@ -25,6 +25,7 @@ namespace RestaurantOrderSystemMVC.Controllers
         {
             ViewData["MenuItem"] = MenuItems;
             ViewData["MenuList"] = new SelectList(_context.Menus, "ItemId", "Name");
+            ViewData["Success"] = false;
             return View();
         }
 
@@ -32,31 +33,52 @@ namespace RestaurantOrderSystemMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Ordering(IFormCollection values)
         {
-            Dictionary<int, int> orderDict = new Dictionary<int, int>();
             Decimal total = Decimal.Parse(values["total"][0]);
-            var orderedItems = values["OrderedItems"];
-            foreach (var item in orderedItems) 
-            {
-                int temp = int.Parse(item);
-                if (orderDict.ContainsKey(temp)) 
-                {
-                    orderDict[temp] += 1;
-                }
-                else 
-                { 
-                    orderDict.Add(temp,1);
-                }
-            }
-           // OrderMain prod = _context.OrderMains.LastOrDefault<OrderMain>();
-            foreach (var item in orderDict) 
+            if (total > 0) 
             { 
-                
+                Dictionary<int, int> orderDict = new Dictionary<int, int>();
+                var orderedItems = values["OrderedItems"];
+                foreach (var item in orderedItems) 
+                {
+                    int temp = int.Parse(item);
+                    if (orderDict.ContainsKey(temp)) 
+                    {
+                        orderDict[temp] += 1;
+                    }
+                    else 
+                    { 
+                        orderDict.Add(temp,1);
+                    }
+                }
+ 
+                OrderMain lastOrderMain = _context.OrderMains.OrderByDescending(p => p.OrderNumber).FirstOrDefault<OrderMain>();
+                int newOrderNumber = lastOrderMain.OrderNumber + 1;
+                foreach (var item in orderDict) 
+                {
+                    OrderMain newOrder = new OrderMain();
+                    newOrder.OrderNumber = newOrderNumber;
+                    newOrder.ItemId = item.Key;
+                    newOrder.Quantity = item.Value;
+                    newOrder.DateTimePlaced = DateTime.Now;
+                    newOrder.OrderStatus = "Placed";
+                    _context.OrderMains.Add(newOrder);
+                }
+
+                _context.SaveChanges();
+
+                ViewData["MenuItem"] = MenuItems;
+                ViewData["MenuList"] = new SelectList(_context.Menus, "ItemId", "Name");
+                ViewData["Success"] = true;
+                return View();
             
             }
-            //ViewData["MenuData"] = _context.Menus.ToList();
-            //ViewData["MenuList"] = new SelectList(_context.Menus, "ItemId", "Name");
-            //return View();
-            return RedirectToAction("Ordering");
+            else 
+            {
+                ViewData["MenuItem"] = MenuItems;
+                ViewData["MenuList"] = new SelectList(_context.Menus, "ItemId", "Name");
+                ViewData["Success"] = false;
+                return View();
+            }
         }
     }
 }
